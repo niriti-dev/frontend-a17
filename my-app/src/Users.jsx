0fd06e6a1185c63b7a77658163f154b238d0fb73
Aadit Grover <aaditgrover@10-18-148-192.dynapool.wireless.nyu.edu>
2025-04-14 18:09:54 -0400

    users css implemented

--- my-app/src/Users.jsx
+++ my-app/src/Users.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Users.css';
 
 function Users() {
   const users = [
@@ -8,7 +9,7 @@ function Users() {
   ];
 
   return (
-    <div>
+    <div className="table-container">
       <h2>Users List</h2>
       <table border="1" cellPadding="8">
         <thead>
